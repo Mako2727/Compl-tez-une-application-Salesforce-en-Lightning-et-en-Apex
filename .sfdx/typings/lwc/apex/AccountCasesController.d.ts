86359fe9759131fef7f8bad6222e8664f5ee52a9
Mako2727 <mkordo1@yahoo.fr>
2024-10-27 00:35:31 +0200

    Retrait du repertoire SFDX
    Retrait du repertoire SFDX

--- .sfdx/typings/lwc/apex/AccountCasesController.d.ts
+++
@@ -1,3 +0,0 @@
-declare module "@salesforce/apex/AccountCasesController.findCasesBySubject" {
-  export default function findCasesBySubject(param: {searchTerm: any, accountId: any}): Promise<any>;
-}
